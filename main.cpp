4abc0c5c672e8c0ee15f36c883f487331111162f
Emot7 <ewidmer2006@icloud.com>
2026-08-07 10:40:40 -0700

    Add Food class with texture loading

--- main.cpp
+++ main.cpp
@@ -1,15 +1,50 @@
 #include <raylib.h>
 
+int cellSize = 30;
+int cellCount = 25;
+Color lightBeige = { 198, 186, 172, 255 };
+Color darkPurple = { 30, 28, 50, 255 };
+
+class Food {
+
+public:
+    Vector2 position = {5, 6};
+
+    Texture2D texture;
+
+    Food()
+    {
+        Image image = LoadImage ("food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+    }
+
+    ~Food()
+    {
+        UnloadTexture(texture);
+    }
+
+    void Draw()
+    {
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+    }
+
+};
+
 int main()
 {
-    InitWindow(750, 750, "Retro Snake");
+    
+    InitWindow(cellSize*cellCount, cellSize*cellCount, "Retro Snake");
     SetTargetFPS(60);
+
+    Food food = Food();
     
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLUE);
-        DrawRectangle(100, 100, 200, 200, RED);
+        ClearBackground(lightBeige);  
+        food.Draw();
+
         EndDrawing();
     }
     
