71b423b2cbae8ecb5e2c1455f758802d71618dd0
Emot7 <ewidmer2006@icloud.com>
2026-08-09 16:08:55 -0700

    Add snake movement and timing system

--- main.cpp
+++ main.cpp
@@ -1,24 +1,47 @@
 #include <raylib.h>
 #include <deque>
+#include <raymath.h>
 
 int cellSize = 30;
 int cellCount = 25;
 Color lightBeige = { 198, 186, 172, 255 };
 Color darkPurple = { 30, 28, 50, 255 };
 
+double lastUpdateTime = 0;
+
+bool eventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if(currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
 class Snake
 {
     public :
         std::deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
 
+        Vector2 direction = {1,0};
+
         void Draw()
         {
-            for(int i = 0; i < body.size(); i++)
+            for(unsigned int i = 0; i < body.size(); i++)
             {
-                int x = body[i].x;
-                int y = body[i].y;
-                DrawRectangle(x*cellSize, y*cellSize, cellSize, cellSize, darkPurple);
+                float x = body[i].x;
+                float y = body[i].y;
+                Rectangle segment = Rectangle{(float)x * cellSize, (float)y * cellSize, (float)cellSize, (float)cellSize};
+                DrawRectangleRounded(segment, 0.5, 6, darkPurple);
             }
+        }
+    
+        void Update()
+        {
+            body.pop_back();
+            body.push_front(Vector2Add(body[0], direction));
         }
 };
 
@@ -67,6 +90,12 @@ int main()
     while (!WindowShouldClose())
     {
         BeginDrawing();
+
+        if(eventTriggered(0.2))
+        {
+            snake.Update();
+        }
+
         ClearBackground(lightBeige);  
         food.Draw();
         snake.Draw();
